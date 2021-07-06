2b8617299410011745f8efb5caf0c92d3577f661
xuesheng <xuesheng@shein.com>
2021-07-06 22:40:08 +0800

    correct producer/consumer solution

--- chapter-30/producer_consumer.c
+++ chapter-30/producer_consumer.c
@@ -4,10 +4,15 @@
 #include <pthread.h>
 #include "../include/thread.h"
 
-int buffer;
+#define LOOPS 101
+#define MAX 10
+
+int buffer[MAX];
+int fill_ptr = 0;
+int use_ptr = 0;
 int count = 0; //默认表示buffer中为空。
-int loops;
-pthread_cond_t cond;
+int loops = LOOPS;
+pthread_cond_t empty, fill;
 pthread_mutex_t mutex;
 
 void init();
@@ -18,42 +23,47 @@ void *consumer(void *arg);
 
 int main(int argc, char const *argv[])
 {
-    pthread_t t1, t2;
-    Pthread_create(&t1, NULL, producer, NULL);
-    Pthread_create(&t2, NULL, consumer, NULL);
+    init();
+    pthread_t t1, t2, t3;
+    Pthread_create(&t1, NULL, producer, "producer 0");
+    Pthread_create(&t2, NULL, consumer, "consumer 0");
+    Pthread_create(&t3, NULL, consumer, "consumer 1");
     Pthread_join(t1, NULL);
     Pthread_join(t2, NULL);
+    Pthread_join(t3, NULL);
     return 0;
 }
 
 void init() {
     Pthread_mutex_init(&mutex, NULL);
-    Pthread_cond_init(&cond, NULL);
-    loops = 1000;
+    Pthread_cond_init(&empty, NULL);
+    Pthread_cond_init(&fill, NULL);
 }
 
 void put(int value) {
-    assert(count == 0);
-    count = 1;
-    buffer = value;
+    buffer[fill_ptr] = value;
+    fill_ptr = (fill_ptr + 1) % MAX;
+    count++;
 }
 
 int get() {
-    assert(count == 1);
-    count = 0;
-    return buffer;
+    int tmp = buffer[use_ptr];
+    use_ptr = (use_ptr + 1) % MAX;
+    count--;
+    return tmp;
 }
 
 void *producer(void *arg) {
     for (int i = 0; i < loops; i++)
     {
         Pthread_mutex_lock(&mutex);
-        if (count == 1)
-            Pthread_cond_wait(&cond, &mutex);
+        while (count == MAX)
+            Pthread_cond_wait(&empty, &mutex);
 
         put(i);
-        Pthread_cond_signal(&cond);
+        Pthread_cond_signal(&fill);
         Pthread_mutex_unlock(&mutex);
+        printf("%s %d\n", arg, i);
     }
     return NULL;
 }
@@ -62,13 +72,13 @@ void *consumer(void *arg) {
     for (int i = 0; i < loops; i++)
     {
         Pthread_mutex_lock(&mutex);
-        if (count == 0)
-            Pthread_cond_wait(&cond, &mutex);
+        while (count == 0)
+            Pthread_cond_wait(&fill, &mutex);
 
-        int tmp = get();    
-        Pthread_cond_signal(&cond);
+        int tmp = get();
+        Pthread_cond_signal(&empty);
         Pthread_mutex_unlock(&mutex);
-        printf("%d\n", tmp);
+        printf("%s %d\n", arg, tmp);
     }
     return NULL;
 }
